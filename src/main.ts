b9cc0f53469f42f1a179df70f4004fce4414af4c
Swappnet <advance11ua@gmail.com>
2023-08-10 18:09:03 +0300

    fix build

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,10 @@ import './styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Toast from 'vue-toastification'
 import App from './App.vue'
-
-import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(Toast)
 
 app.mount('#app')
