9572c080d02fc66671955f9d76e1db0c98e49ee7
Swappnet <advance11ua@gmail.com>
2023-08-10 22:24:50 +0300

    remove db click

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// RESETS
+
+document.ondblclick = function (e) {
+  e.preventDefault()
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
