3882029c637c8bb1e70795b4ae467c5c0090519c
Swappnet <advance11ua@gmail.com>
2023-08-10 22:28:10 +0300

    fix double zoom

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-// RESETS
-
-document.ondblclick = function (e) {
-  e.preventDefault()
-}
-
 const app = createApp(App)
 
 app.use(createPinia())
