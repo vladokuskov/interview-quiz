077100fdda543da8440f276e71e3a63ca032d879
Swappnet <advance11ua@gmail.com>
2023-08-25 13:49:44 +0300

    refactored current topic section

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -16,10 +16,10 @@ const { color = 'neutral' } = toRefs(props)
     <button :class="clsx(
         'font-inria text-black tracking-tight inline-flex items-center justify-center gap-2 flex-wrap transition-all rounded-xl font-semibold py-2 px-6 disabled:bg-neutral-400 border-2 border-black shadow-brutalism',
         'active:shadow-brutalism-sm focus:shadow-brutalism',
-        color === 'blue' ? 'bg-blue-400 hover:bg-blue-300 focus:bg-blue-300' :
-            color === 'red' ? 'bg-red-500 hover:bg-red-400 focus:bg-red-400' :
-                color === 'lime' ? 'bg-lime-400 hover:bg-lime-300 focus:bg-lime-300' :
-                    color === 'yellow' ? 'bg-yellow-400 hover:bg-yellow-300 focus:bg-yellow-300' :
+        color === 'blue' ? 'bg-blue-400 hover:bg-blue-500 focus:bg-blue-500' :
+            color === 'red' ? 'bg-red-500 hover:bg-red-600 focus:bg-red-600' :
+                color === 'lime' ? 'bg-lime-400 hover:bg-lime-500 focus:bg-lime-500' :
+                    color === 'yellow' ? 'bg-yellow-400 hover:bg-yellow-500 focus:bg-yellow-500' :
                         '',
     )">
         <slot></slot>
